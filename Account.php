c976792bb6b1fef27700d332b76fc7e0599d4976
Lclarke277 <Lclarke@unca.edu>
2016-05-24 12:31:12 -0400

    Now saves and displays notes

--- Account.php
+++ Account.php
@@ -1,17 +1,28 @@
-<!-- This page will be used in the future for when 
-     you are logged in. It is where most of our site
-     will take place. Wheres users notes are saved, created,
-     and edited -->
 
 <?php require 'connections.php'; ?>
 <?php
 session_start();
-if(isset($_SESSION["UserID"])){    
+if(isset($_SESSION["UserID"])){ 
 }
 
 else {
     header('Location: Login.php');
-}
+}?>
+<?php 
+    
+    if(isset($_POST['save'])) {
+       
+        session_start();
+        $note = $_POST['note'];
+        $UserID = ($_SESSION["UserID"]);
+        
+        $sql = $con->query("INSERT INTO notes (UserID, note)Values('{$UserID}','{$note}')");
+        
+        header('Location: Account.php');
+    }
+?>
+<?php 
+    $result = mysqli_query($con, "SELECT * FROM notes WHERE UserID = '5'");
 ?>
 
 <!DOCTYPE HTML>
@@ -23,11 +34,28 @@ else {
     
     <body>
          
-        <h2>Welcome to your account</h2>
-        <a href="Logout.php">Logout</a>
-    
+        <h2>Welcome</h2>
+        
+        <form action="" method="post" name="notesubmit" id="notesubmit">
+                <div>
+                    <input name="note" class="note" type="text" required="required"  id="note" placeholder="Type A New Note">
+                </div>
+                <div>
+                    <input name="save"  type="submit" class="button" id="save" value="Save">
+                </div>
+        </form>
+        
+            <?php while ($row = mysqli_fetch_array($result)): ?>
+                <?php $note = $row['note']; ?>
+
+        <div><p class="note"><?php echo $note; ?></p></div>
+        
+            <?php endwhile; ?>
+        
+        
+        <div>    
+            <a href="Logout.php">Logout</a>
+        </div>
+        
     </body>
-
-
-
 </html>
